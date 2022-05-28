76c379438865373e44b9f786b8c1e50a8ca007a6
Tom Willis <twillis@flexion.us>
2022-05-28 11:19:38 -0500

    refactor test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,30 +1,63 @@
 #!/bin/bash
 #
 # Flip the mode value to control the --dryRun flag
-CONTAINER=issue-test
-docker build . -t "$CONTAINER"
+
+create_docker_image() {
+  TEST_IMAGE=issue-test
+  docker build . -t "$TEST_IMAGE"
+  echo $TEST_IMAGE
+}
+
+run_docker() {
+  docker run --rm -v "$(pwd)/test":/app/test "$1" "$2" fake-password "$OWNER/$REPO" "$BRANCH" "$TITLE" "$LABELS" "$MODE" 2>&1 | tee "$OUTPUTS_FILE"
+  echo "$OUTPUTS_FILE"
+}
+
+test_string() {
+  if [ "$1" = "$DRY_RUN" ]; then
+    echo "## Results"
+  else
+    echo "HttpError: Bad credentials"
+  fi
+}
+
+test_result() {
+  if grep -Fxq "$TEST_STRING" "$OUTPUTS_FILE"; then
+    echo
+    echo "✅ Test result: passes"
+  else
+    echo
+    echo "❌ Test result: fails"
+    exit 1
+  fi
+}
+
+run_test() {
+  run_docker "$IMAGE" "$1"
+  TEST_STRING=$(test_string "$MODE")
+  test_result "$TEST_STRING"
+}
+
+###
+# Script starts here
+###
 export DRY_RUN="true"
 export LIVE_RUN="false"
 MODE=$DRY_RUN
 OUTPUTS_FILE=./test/test-outputs.txt
-FIXTURE_FILE=./test/fixtures/codeql.sarif
+#FIXTURE_FILE=./test/fixtures/codeql.sarif
+# FIXTURE_FILE=./test/fixtures/odc.sarif
 OWNER=tomwillis608
 REPO=sarif-to-issue-action
 BRANCH=fake-test-branch
 TITLE="Test security issue from build"
 LABELS="build"
-docker run --rm -v "$(pwd)/test":/app/test "$CONTAINER" "$FIXTURE_FILE" fake-password "$OWNER/$REPO" "$BRANCH" "$TITLE" "$LABELS" "$MODE" 2>&1 | tee $OUTPUTS_FILE
-if [ "$MODE" = "$DRY_RUN" ]; then
-  TEST_STRING="## Results"
-else
-  TEST_STRING="HttpError: Bad credentials"
-fi
-if grep -Fxq "$TEST_STRING" "$OUTPUTS_FILE"; then
-  echo
-  echo "✅ Test result: passes"
 
-else
-  echo
-  echo "❌ Test result: fails"
-  exit 1
-fi
+rm -f $OUTPUTS_FILE
+IMAGE=$(create_docker_image)
+# shellcheck disable=SC2043
+# remove this disable when we loop over multiple files
+for testfile in "./test/fixtures/codeql.sarif"; do
+  echo "$testfile"
+  run_test "$testfile"
+done
